a8b1465e75fb24b429434995c427d37c52f84daa
sothx <i@sothx.com>
2025-03-08 02:12:03 +0800

    feat: update

--- action.sh
+++ action.sh
@@ -1,2 +1,2 @@
 #!/system/bin/sh
-am start -n io.github.a13e300.ksuwebui/io.github.a13e300.ksuwebui.WebUIActivity --es id "hyper-zram-writeback-web-ui"
+am start -n io.github.a13e300.ksuwebui/io.github.a13e300.ksuwebui.WebUIActivity --es id "hyper-video-wallpaper-loop-web-ui"
